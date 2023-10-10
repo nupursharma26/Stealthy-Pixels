ede127d62f20a70733e109aac61f9221f3b31c7f
nupursharma26 <81218174+nupursharma26@users.noreply.github.com>
2023-10-11 01:39:14 +0530

    Add files via upload

---
+++ src/Pixel.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+public class Pixel {
+
+    public Pixel(int x, int y, Color colorToAdd) {
+    }
+
+    public void setColor(Color getNewPixelColor) {
+    }
+
+    public Color getColor() {
+        return null;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+}
